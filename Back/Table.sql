c05948c874e40d796925d0c360734045d365eb9a
Jun Hyuk Lim <limjunhyuk97@naver.com>
2021-09-25 17:26:55 +0900

    Update Table.sql

--- Back/Table.sql
+++ Back/Table.sql
@@ -1,5 +1,4 @@
 # Table 생성
-
 CREATE TABLE `table_name`
 (
 	col_name1 char(8) NOT NULLL,
@@ -10,10 +9,25 @@ CREATE TABLE `table_name`
 
 
 # 데이터 검색
+SELECT cols FROM table_name WHERE condition;
 
-SELECT * FROM table_name WHERE condition;
-SELECT * FROM table_name WHERE col_name1="..."
+## 예시
+SELECT cols FROM table_name WHERE col_name1="..."
+
+
+# 데이터 추가
+INSERT INTO table_name VALUES(...);
+
+
+# 데이터 변경
+UPDATE table_name SET targetcol_name=.. WHERE keycol_name=..
+
+
+# 데이터 삭세
+DELETE FROM target_table WHERE targetcol_name=.. 
+
 
 # 테이블 삭제
-
 DROP TABLE `table_name`;
+
+
