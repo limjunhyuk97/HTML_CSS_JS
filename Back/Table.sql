bd8fc902a7cf6278eae106a3dae44d06895171a6
Jun Hyuk Lim <limjunhyuk97@naver.com>
2021-09-25 17:07:24 +0900

    Update Table.sql

--- Back/Table.sql
+++ Back/Table.sql
@@ -1,19 +1,19 @@
 # Table 생성
 
-create table `table_name`
+CREATE TABLE `table_name`
 (
 	col_name1 char(8) NOT NULLL,
   	col_name2 char(5),
   	col_name3 int,
- 	 PRIMARY KEY(col_name)
+	PRIMARY KEY(col_name)
 );
 
 
 # 데이터 검색
 
-Select * from table_name where condition;
-Select * from table_name where col_name1="..."
+SELECT * FROM table_name WHERE condition;
+SELECT * FROM table_name WHERE col_name1="..."
 
 # 테이블 삭제
 
-drop table `table_name`;
+DROP TABLE `table_name`;
