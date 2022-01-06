528fdd92f1d1158b42ba63d8dfbf03a261cf22d5
jun_lim97 <limjunhyuk97@gmail.com>
2022-01-06 23:39:02 +0900

    22.01.06 학습 정리

---
+++ Web01/main.js
@@ -0,0 +1,16 @@
+const boxEl = document.querySelector('.box');
+
+console.log(boxEl.classList);
+
+boxEl.addEventListener('click', function() {
+  if(boxEl.classList.contains('active')) {
+    boxEl.classList.remove('active');
+    console.log(boxEl.classList);
+  }
+  else{
+    boxEl.classList.add('active');
+    console.log(boxEl.classList);
+  }
+  console.log('clicked!');
+});
+
